da754bac706e785bffd41be8f15b4506c47005a8
IvanovJenyIT <ivanovjenyit@gmail.com>
2023-12-14 17:06:25 +0200

    CalldataMemory

---
+++ contracts/CalldataMemory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.0 <0.9.0;
+
+contract CalldataMemory {
+  function work(string memory _str) external pure returns(bytes32 data) {
+    assembly {
+      let ptr := mload(64)
+      data := mload(sub(ptr, 32))
+    }
+  }
+
+  function sel() external pure returns (bytes4) {
+    return bytes4(keccak256(bytes("callData(uint256[3])")));
+  }
+
+  function callData(uint[3] calldata _arr) external pure returns(bytes32 _el1) {
+    assembly{
+      _el1 := calldataload(4)
+    }
+  }
+} 
